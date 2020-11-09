86285410615b7bfdc92ab60666fc7ecab33bab09
DengTianjian <mail@isdtj.com>
2020-11-09 18:23:19 +0800

    修改全局圆角值

--- theme.js
+++ theme.js
@@ -1,6 +1,7 @@
 module.exports = {
   "primary-color": "#ff4141",
   "link-color": "#ff4141",
-  "border-radius-base": "2px",
+  "border-radius-base": "4px",
+  "border-radius-sm":"2px",
   "text-color":"#999"
 };
