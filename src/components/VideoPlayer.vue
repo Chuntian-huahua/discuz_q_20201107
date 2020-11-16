daddb22fe9089d4286d8f347820d838029abf84e
DengTianjian <mail@isdtj.com>
2020-11-16 16:04:24 +0800

    增加 腾讯云点播API

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,16 +1,7 @@
 <template>
-  <div class="video-player">
-    <div class="big-play-button" @click="playVideo" v-show="alreadyStarted===false||playing===false">
-      <c-icon :value="playing?'icon-zanting':'icon-playfill'" size="40px" @click="playVideo"></c-icon>
-    </div>
-    <video
-      class="video"
-      id="player-container-id"
-      preload="auto"
-      playsinline
-      webkit-playsinline
-      x5-playsinline
-    ></video>
+  <div class="video-player" v-show="show">
+    <slot />
+    <video class="video" :id="videoId" preload="auto" playsinline webkit-playsinline x5-playsinline></video>
   </div>
 </template>
 
@@ -19,15 +10,103 @@ let TCPLayer = null;
 export default {
   props: {
     fileid: String,
-    appid:String|Number
+    appid: String | Number,
+    controls: {
+      type: Boolean,
+      default: true,
+    },
+    bigPlayButton: {
+      type: Boolean,
+      default: true,
+    },
+    autoplay: {
+      type: Boolean,
+      default: false,
+    },
+    loop: {
+      type: Boolean,
+      default: false,
+    },
+    muted: {
+      type: Boolean,
+      default: false,
+    },
+    preload: {
+      type: String,
+      default: "auto",
+    },
+    swf: {
+      type: String,
+      default: "",
+    },
+    posterImage: {
+      type: Boolean,
+      default: true,
+    },
+    language: {
+      type: String,
+      default: "zh-CN",
+    },
+    languages: {
+      type: Object,
+      default: null,
+    },
+    controlBar: {
+      type: Object,
+      default: null,
+    },
+    plugins: {
+      type: Object,
+      default: null,
+    },
+    hlsConfig: {
+      type: Object,
+      default: null,
+    },
+    currentTime: {
+      type: Number,
+      default: null,
+    },
+    dispose: {
+      type: Boolean,
+      default: false,
+    },
+    play: {
+      type: Boolean,
+      default: false,
+    },
+    pause: {
+      type: Boolean,
+      default: false,
+    },
+    volume: {
+      type: Array,
+      default: null,
+    },
+    poster: {
+      type: String,
+      default: null,
+    },
+    requestFullscreen: {
+      type: Boolean,
+      default: null,
+    },
+    exitFullscreen: {
+      type: Boolean,
+      default: null,
+    },
   },
   data() {
     return {
+      show: true,
       alreadyStarted: true,
       playing: false,
+      fullscreen: false,
+      videoId: Date.now(),
     };
   },
   created() {
+    this.videoId = "tcbVideo_" + Date.now();
     if (TCPLayer) {
       TCPLayer.dispose();
     }
@@ -37,19 +116,200 @@ export default {
   },
   methods: {
     reloadPlayer() {
-      TCPLayer = TCPlayer("player-container-id", {
-        /**player-container-id 为播放器容器ID，必须与html中一致*/
-        fileID: this.fileid /**请传入需要播放的视频fileID 必须 */,
-        appID:this.appid /**请传入点播账号的appID 必须 */,
+      let options = {
+        fileID: this.fileid,
+        appID: this.appid,
         psign: "",
-        bigPlayButton: false,
-        /**其他参数请在开发文档中查看 */
+        bigPlayButton: this.bigPlayButton,
+        controls: this.controls,
+        autoplay: this.autoplay,
+        loop: this.loop,
+        muted: this.muted,
+        preload: this.preload,
+        swf: this.swf,
+        posterImage: this.posterImage,
+        language: this.language,
+      };
+      if (this.languages) {
+        options["languages"] = this.languages;
+      }
+      if (this.controlBar) {
+        options["controlBar"] = this.controlBar;
+      }
+      if (this.plugins) {
+        options["plugins"] = this.plugins;
+      }
+      if (this.hlsConfig) {
+        options["hlsConfig"] = this.hlsConfig;
+      }
+      TCPLayer = TCPlayer(this.videoId, options);
+      if (this.currentTime !== null) {
+        TCPLayer.currentTime(this.currentTime);
+      }
+      const that = this;
+
+      TCPLayer.on("play", function () {
+        this.playing = true;
+        that.$emit("play", arguments);
       });
+      TCPLayer.on("ready", function () {
+        that.$emit("duration", TCPLayer.duration());
+        if (that.$listeners.hasOwnProperty("ready")) {
+          that.$emit("ready", arguments);
+        }
+      });
+      if (this.poster) {
+        TCPLayer.poster(this.poster);
+        this.$emit("getPoster", TCPLayer.poster());
+      }
+      if (this.volume) {
+        TCPLayer.volume(this.volume);
+      }
+      if (this.$listeners.hasOwnProperty("playing")) {
+        TCPLayer.on("playing", function () {
+          that.$emit("playing", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("loadstart")) {
+        TCPLayer.on("loadstart", function () {
+          that.$emit("loadstart", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("durationchange")) {
+        TCPLayer.on("durationchange", function () {
+          that.$emit("durationchange", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("loadedmetadata")) {
+        TCPLayer.on("loadedmetadata", function () {
+          that.$emit("loadedmetadata", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("loadeddata")) {
+        TCPLayer.on("loadeddata", function () {
+          that.$emit("loadeddata", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("progress")) {
+        TCPLayer.on("progress", function () {
+          that.$emit("progress", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("canplay")) {
+        TCPLayer.on("canplay", function () {
+          that.$emit("canplay", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("canplaythrough")) {
+        TCPLayer.on("canplaythrough", function () {
+          that.$emit("canplaythrough", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("error")) {
+        TCPLayer.on("error", function () {
+          that.$emit("error", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("pause")) {
+        TCPLayer.on("pause", function () {
+          that.$emit("pause", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("ratechange")) {
+        TCPLayer.on("ratechange", function () {
+          that.$emit("ratechange", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("seeked")) {
+        TCPLayer.on("seeked", function () {
+          that.$emit("seeked", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("seeking")) {
+        TCPLayer.on("seeking", function () {
+          that.$emit("seeking", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("timeupdate")) {
+        TCPLayer.on("timeupdate", function () {
+          that.$emit("timeupdate", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("volumechange")) {
+        TCPLayer.on("volumechange", function () {
+          that.$emit("volumechange", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("waiting")) {
+        TCPLayer.on("waiting", function () {
+          that.$emit("waiting", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("ended")) {
+        TCPLayer.on("ended", function () {
+          that.$emit("ended", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("resolutionswitching")) {
+        TCPLayer.on("resolutionswitching", function () {
+          that.$emit("resolutionswitching", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("resolutionswitched")) {
+        TCPLayer.on("resolutionswitched", function () {
+          that.$emit("resolutionswitched", arguments);
+        });
+      }
+      if (this.$listeners.hasOwnProperty("fullscreenchange")) {
+        TCPLayer.on("fullscreenchange", function () {
+          that.$emit("fullscreenchange", arguments);
+        });
+      }
     },
     playVideo() {
       this.playing = true;
-      this.alreadyStarted=true;
+      this.alreadyStarted = true;
       TCPLayer.play();
+    },
+  },
+  watch: {
+    controls() {
+      this.show = false;
+      if (this.controls) {
+        TCPLayer.controlBar.el_.style.visibility = "visible";
+      } else {
+        TCPLayer.controlBar.el_.style.visibility = "hidden";
+      }
+      setTimeout(() => {
+        this.show = true;
+      }, 200);
+    },
+    play() {
+      if (this.play === true) {
+        TCPLayer.play();
+      }
+    },
+    pause() {
+      if (this.pause == true) {
+        TCPLayer.pause();
+      }
+    },
+    requestFullscreen() {
+      if (this.requestFullscreen === true) {
+        TCPLayer.requestFullscreen();
+        this.$emit("isFullscreen", TCPLayer.isFullscreen());
+      }
+    },
+    exitFullscreen() {
+      if (this.exitFullscreen === true) {
+        TCPLayer.exitFullscreen();
+        this.$emit("isFullscreen", TCPLayer.isFullscreen());
+      }
+    },
+    dispose() {
+      if (this.dispose === true) {
+        TCPLayer.dispose();
+      }
     },
   },
 };
@@ -58,24 +318,11 @@ export default {
 <style scoped>
 .video-player {
   position: relative;
-}
-.big-play-button {
-  position: absolute;
-  z-index: 1;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  padding: 10px 22px;
-  width: 80px;
-  height: 80px;
-  margin: auto;
-  color: white;
-  background: rgba(0, 0, 0, 0.38);
-  border-radius: 50%;
-  cursor: pointer;
+  background: black;
 }
 .video {
+  position: relative;
+  z-index: 9;
   width: 100%;
   height: 100%;
   overflow: hidden;
